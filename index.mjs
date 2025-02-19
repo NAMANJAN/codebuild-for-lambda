8628de27c58868927d2f6b85bb79725eee0b8bea
NAMANJAN <85395393+NAMANJAN@users.noreply.github.com>
2025-02-19 13:07:55 +0530

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ export const handler = async (event) => {
   // TODO implement
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello Naman Jain from Lambda!'),
+    body: JSON.stringify('Naman from Lambda!'),
   };
   return response;
 };
